abb66b27b7e72dea6a4a9fa9ad551df4464e7580
Adam Langbert <adamhl@pm.me>
2026-07-12 22:03:49 -0500

    chore(deps): update dependencies

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,6 @@
 import { tsdownConfig } from "@adamhl8/configs"
 import { defineConfig } from "tsdown"
 
-const config = tsdownConfig({ platform: "neutral" })
+const config = tsdownConfig({ platform: "neutral", failOnWarn: false })
 
 export default defineConfig(config)
