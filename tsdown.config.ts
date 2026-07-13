696bb1d260f6b0e5a5809f39cffbe25b08471bbe
Adam Langbert <adamhl@pm.me>
2026-07-12 23:19:19 -0500

    chore(tsdown): remove failOnWarn override

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,6 @@
 import { tsdownConfig } from "@adamhl8/configs"
 import { defineConfig } from "tsdown"
 
-const config = tsdownConfig({ platform: "neutral", failOnWarn: false })
+const config = tsdownConfig({ platform: "neutral" })
 
 export default defineConfig(config)
